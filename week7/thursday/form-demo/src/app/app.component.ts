3dcef3a4ded35febe71de764e2dd3829bfa3c72d
adithya <adithya.3035@gmail.com>
2024-11-14 19:23:30 -0800

    changes

--- week7/thursday/form-demo/src/app/app.component.ts
+++ week7/thursday/form-demo/src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-  
-  ],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'form-demo';
+
+  
+
+  onSubmit(formData: NgForm) {
+    console.log(formData.form.value);
+    formData.form.reset();
+   
+  }
 }
